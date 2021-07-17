7fdff1e3e5abfbeb7e0d5cd319915a8b74e4740d
HanselV <hanselviva@gmail.com>
2021-07-17 13:15:46 -0700

    index & server done

--- api/server.js
+++ api/server.js
@@ -1,9 +1,40 @@
-const express = require('express');
+/* eslint-disable no-unused-vars */
+const express = require("express");
 const server = express();
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+const helmet = require("helmet");
+const morgan = require("morgan");
+const actionsRouter = require("./actions/actions-router");
+const projectsRouter = require("./projects/projects-router");
+
+server.use(express.json());
+server.use(helmet());
+server.use(morgan("dev"));
+
+server.use("/api/projects", projectsRouter);
+server.use("api/actions", actionsRouter);
+
+// root handler
+server.get("/", (req, res) => {
+	res.send(`<h2> Welcome to my API Homepage! </h2>`);
+});
+
+// catch all
+server.use("*", (req, res) => {
+	res.status(500).json({ message: "that place doesn't exist" });
+});
+
+// error handler
+server.use((err, req, res, next) => {
+	const message = err?.message || "something went wrong in the server";
+	const status = err?.status || 500;
+
+	res.status(`${status}`).json({ message, stack: err.stack });
+});
 
 module.exports = server;
